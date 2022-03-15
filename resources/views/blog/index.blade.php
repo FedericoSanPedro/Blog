b070338d41174c98bf378d81b959ad1cac392111
Federico <fedu_zero@hotmail.com>
2022-03-14 21:09:50 -0300

    index method

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,25 +9,32 @@
     </div>
 </div>
 
-<div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
-    <div>
-        <img src="https://cdn.pixabay.com/photo/2017/11/27/21/31/computer-2982270_960_720.jpg" width="700" alt="">
-    </div>
-    <div>
-        <h2 class="text-gray-700 font-bold text-5xl pb-4">
-            Learn how to write in Laravel code
-        </h2>
-        <span class="text-gray-500">
-            By <span class="font-bold italic text-gray-800">Code With Dary</span>
-        </span>, 1 day ago
-        <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere repudiandae similique optio deleniti quo incidunt, blanditiis accusantium est sint iste omnis totam adipisci, cumque hic quia asperiores cum repellat earum.
-        </p>
+@foreach ($posts as $post)
+    
 
-        <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-            Keep reading
-        </a>
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+            <img src="https://cdn.pixabay.com/photo/2017/11/27/21/31/computer-2982270_960_720.jpg" width="700" alt="">
+        </div>
+        <div>
+            <h2 class="text-gray-700 font-bold text-5xl pb-4">
+                {{ $post->title }}
+            </h2>
+            <span class="text-gray-500">
+                By <span class="font-bold italic text-gray-800">
+                    {{ $post->user->name }}
+                </span>
+            </span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                {{ $post->description }}
+             </p>
+
+            <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                Keep reading
+            </a>
+        </div>
     </div>
-</div>
+
+@endforeach
 
 @endsection
